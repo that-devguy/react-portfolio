d677fbebfa4fb5681f3dc6c3f7a7d314b620fe47
that devguy <zkmutch@gmail.com>
2023-06-16 19:24:08 -0400

    rebuilt the app with next.js and tailwindcss

---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>that_devguy React Portfolio</h1>
+  )
+}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(
-    <h1>Hello, this is my portfolio</h1>,
-    document.getElementById("root")
-)
