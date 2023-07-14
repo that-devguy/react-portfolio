6e406f5b097bd81d9a92db904d75a145e61614e2
that devguy <zkmutch@gmail.com>
2023-07-14 16:27:37 -0400

    added OIO Dev, my final OSU Bootcamp group project to my portfolio

--- src/app/projects/data.js
+++ src/app/projects/data.js
@@ -2,6 +2,16 @@
 export default [
   {
     id: "1",
+    title: "OIO Dev",
+    description:
+      "Our full-stack web application empowers our team of developers to market their services, allowing users to conveniently browse portfolios, create accounts, and book consultations with a freelance developer. We've streamlined the consultation process by integrating the Calendly API for easy scheduling.",
+    techList: ["Next.js", "React", "Javascript", "Tailwind", "MongoDB", "GraphQL"],
+    imageUrl: "/images/oiodev-demo.png",
+    appUrl: "https://www.oiodev.com/",
+    gitHubUrl: "https://github.com/CodeByDex/OIODev",
+  },
+  {
+    id: "2",
     title: "Put A Spork In It",
     description:
       "The virtual kitchen for pop culture fans! A recipe web app that brings the magic of your favorite TV shows, movies, and video games to your plate, with mouth-watering recipes. Create accounts, share your recipes, and save your favorites.",
@@ -11,7 +21,7 @@ export default [
     gitHubUrl: "https://github.com/CodeByDex/PutASporkInIt",
   },
   {
-    id: "2",
+    id: "3",
     title: "Pokédex Portal",
     description:
       "A Pokedex web app with a comprehensive database of all 1008 Pokemon from all 9 generations. It combines data from PokeAPI and Pokemon TCG API to provide users with information about Pokemon, including their stats, evolutions, and trading cards. We feature an elegant design using the Tailwind and offer an intuitive interface for easy searching and accessing of Pokemon information.",
@@ -21,20 +31,10 @@ export default [
     gitHubUrl: "https://github.com/that-devguy/Pokedex-Web-Application",
   },
   {
-    id: "3",
-    title: "Travel Journal",
-    description:
-      "A dynamic web application that combines a beautifully designed interface from Figma with the power of React. By leveraging a data.js file, the application populates components dynamically, showcasing my ability to transform design concepts into a fully functional single-page application.",
-    techList: ["React", "JavaScript", "CSS"],
-    imageUrl: "/images/traveljournal-demo.png",
-    appUrl: "https://that-devguy.github.io/travel-journal/",
-    gitHubUrl: "https://github.com/that-devguy/travel-journal",
-  },
-  {
     id: "4",
     title: "Remote",
     description:
-      "A multi-page web application that demonstrates how Remote can bring ideas to life with exceptional product design and prototyping. The app also simplifies and streamlines the quoting process for ease of use.",
+      "A multi-page web application that demonstrates how Remote can bring ideas to life with exceptional product design and prototyping. This application simplifies and streamlines the quoting process for ease of use.",
     techList: ["JavaScript", "CSS", "HTML", "Bootstrap"],
     imageUrl: "/images/remote-demo.png",
     appUrl: "https://remotedesign.tech/",
@@ -42,13 +42,13 @@ export default [
   },
   {
     id: "5",
-    title: "Coming Soon",
+    title: "Travel Journal",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "A dynamic web application that combines a beautifully designed interface from Figma with the power of React. By leveraging a data.js file, the application populates components dynamically, showcasing my ability to transform design concepts into a fully functional single-page application.",
     techList: ["React", "JavaScript", "CSS"],
-    imageUrl: "/images/placeholder-demo.png",
-    appUrl: "",
-    gitHubUrl: "",
+    imageUrl: "/images/traveljournal-demo.png",
+    appUrl: "https://that-devguy.github.io/travel-journal/",
+    gitHubUrl: "https://github.com/that-devguy/travel-journal",
   },
   {
     id: "6",
