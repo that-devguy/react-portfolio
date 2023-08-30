5c77e3f027c497f80c32022324b8199b98d2e98a
that devguy <zkmutch@gmail.com>
2023-08-30 17:00:11 -0400

    added tactician tracker to my projects section

--- src/app/projects/data.js
+++ src/app/projects/data.js
@@ -2,16 +2,33 @@
 export default [
   {
     id: "1",
+    title: "Tactician Tracker",
+    description:
+      "A dedicated web app designed for avid fans and players of Teamfight Tactics. With the ever-evolving landscape of the game, staying updated with your stats and leaderboards is a top priority. Tactician Tracker bridges this gap by providing real-time statistics, rankings, and insights, leveraging the Riot Games API for accurate and up-to-date data.",
+    techList: ["Next.js", "React", "Javascript", "Tailwind"],
+    imageUrl: "/images/tacticiantracker-demo.png",
+    appUrl: "https://www.tacticiantracker.com/",
+    gitHubUrl: "https://github.com/that-devguy/tactician-tracker",
+  },
+  {
+    id: "2",
     title: "OIO Dev",
     description:
       "Our full-stack web application empowers our team of developers to market their services, allowing users to conveniently browse portfolios, create accounts, and book consultations with a freelance developer. We've streamlined the consultation process by integrating the Calendly API for easy scheduling.",
-    techList: ["Next.js", "React", "Javascript", "Tailwind", "MongoDB", "GraphQL"],
+    techList: [
+      "Next.js",
+      "React",
+      "Javascript",
+      "Tailwind",
+      "MongoDB",
+      "GraphQL",
+    ],
     imageUrl: "/images/oiodev-demo.png",
     appUrl: "https://www.oiodev.com/",
     gitHubUrl: "https://github.com/CodeByDex/OIODev",
   },
   {
-    id: "2",
+    id: "3",
     title: "Put A Spork In It",
     description:
       "The virtual kitchen for pop culture fans! A recipe web app that brings the magic of your favorite TV shows, movies, and video games to your plate, with mouth-watering recipes. Create accounts, share your recipes, and save your favorites.",
@@ -21,7 +38,7 @@ export default [
     gitHubUrl: "https://github.com/CodeByDex/PutASporkInIt",
   },
   {
-    id: "3",
+    id: "4",
     title: "Pokédex Portal",
     description:
       "A Pokedex web app with a comprehensive database of all 1008 Pokemon from all 9 generations. It combines data from PokeAPI and Pokemon TCG API to provide users with information about Pokemon, including their stats, evolutions, and trading cards. We feature an elegant design using the Tailwind and offer an intuitive interface for easy searching and accessing of Pokemon information.",
@@ -31,7 +48,7 @@ export default [
     gitHubUrl: "https://github.com/that-devguy/Pokedex-Web-Application",
   },
   {
-    id: "4",
+    id: "5",
     title: "Remote",
     description:
       "A multi-page web application that demonstrates how Remote can bring ideas to life with exceptional product design and prototyping. This application simplifies and streamlines the quoting process for ease of use.",
@@ -40,16 +57,6 @@ export default [
     appUrl: "https://remotedesign.tech/",
     gitHubUrl: "https://github.com/that-devguy/Remote-Website",
   },
-  // {
-  //   id: "5",
-  //   title: "Coming Soon",
-  //   description:
-  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  //   techList: ["React", "JavaScript", "CSS"],
-  //   imageUrl: "/images/placeholder-demo.png",
-  //   appUrl: "",
-  //   gitHubUrl: "",
-  // },
   // {
   //   id: "6",
   //   title: "Coming Soon",
